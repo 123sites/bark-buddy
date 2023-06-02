2c7bdf5bb85ebc0c32ebab60b7eedfaff9498463
anistone9 <ani.stone.v@gmail.com>
2023-06-01 19:50:33 -0700

    created search page

---
+++ client/src/utils/pupApi.js
@@ -0,0 +1,58 @@
+// route to get logged in user's info (needs the token)
+export const getMe = (token) => {
+    return fetch('/api/users/me', {
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${token}`,
+      },
+    });
+  };
+  
+  export const createUser = (userData) => {
+    return fetch('/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    });
+  };
+  
+  export const loginUser = (userData) => {
+    return fetch('/api/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    });
+  };
+  
+  // save book data for a logged in user
+  export const savePooch = (dogData, token) => {
+    return fetch('/api/users', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(dogData),
+    });
+  };
+  
+  // remove saved book data for a logged in user
+  export const removePooch = (dogId, token) => {
+    return fetch(`/api/users/dogs/${dogId}`, {
+      method: 'DELETE',
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+  };
+  
+  // make a search to google books api
+  // https://www.googleapis.com/books/v1/volumes?q=harry+potter
+  export const searchPooch = (query) => {
+    return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+  };
+  
