2ecfa50f6ea1a82ad0fb95d8d95529125a778993
anistone9 <ani.stone.v@gmail.com>
2023-06-02 22:50:35 -0700

    merge conflicts

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -5,12 +5,30 @@ const petSeeds = require('./petSeeds.json');
 
 db.once('open', async () => {
   try {
+<<<<<<< HEAD
     // await Dog.deleteMany({});
     await User.deleteMany({});
 
     await User.create(userSeeds); 
     // await Dog.create(petSeeds);
 
+<<<<<<< HEAD
+=======
+
+
+
+
+
+
+=======
+    //await Dog.deleteMany({});
+    await User.deleteMany({});
+
+    await User.create(userSeeds); 
+    await Dog.create(petSeeds);
+>>>>>>> cb8671e (changed seeds file)
+
+>>>>>>> 6393d50 (fixing error)
   } catch (err) {
     console.error(err);
     process.exit(1);
