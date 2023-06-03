eca5ce65be291df0ffa81f9ca1eaf1882a7801a4
anistone9 <ani.stone.v@gmail.com>
2023-06-02 22:18:56 -0700

    fixed search list bug

--- client/src/components/DogSearchResult/index.js
+++ client/src/components/DogSearchResult/index.js
@@ -7,11 +7,11 @@ function DogSearchResult({dog}) {
       <div className="px-1 py-1">
       <img 
       alt=''
-      src={dog.primary_photo_cropped?.medium}/>
-      <h1>{dog.name}</h1>
-      <p>Breed: {dog.breeds?.primary}</p>
-      <p>Age: {dog.age}</p>
-      <p>Gender: {dog.gender}</p>
+      src={dog?.primary_photo_cropped?.large}/>
+      <h1>{dog?.name}</h1>
+      <p>Breed: {dog?.breeds?.primary}</p>
+      <p>Age: {dog?.age}</p>
+      <p>Gender: {dog?.gender}</p>
       <button>Save</button>
       <button>Donate</button>
     </div>
