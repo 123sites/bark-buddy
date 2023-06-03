55849c49b6a46abed36552246c08ae7dcf293b14
anistone9 <ani.stone.v@gmail.com>
2023-06-02 21:52:14 -0700

    fixed display bug

--- client/src/components/DogSearchResult/index.js
+++ client/src/components/DogSearchResult/index.js
@@ -4,17 +4,17 @@ import { Link } from "react-router-dom";
 function DogSearchResult({dog}) {
 
     return (
-        <div className="px-1 py-1">
-        <img 
-        alt=''
-        src={dog.primary_photo_cropped?.full}/>
-        <h1>{dog.name}</h1>
-        <p>Breed: {dog.breeds?.primary}</p>
-        <p>Age: {dog.age}</p>
-        <p>Gender: {dog.gender}</p>
-        <button>Save</button>
-        <button>Donate</button>
-      </div>
+      <div className="px-1 py-1">
+      <img 
+      alt=''
+      src={dog.primary_photo_cropped?.medium}/>
+      <h1>{dog.name}</h1>
+      <p>Breed: {dog.breeds?.primary}</p>
+      <p>Age: {dog.age}</p>
+      <p>Gender: {dog.gender}</p>
+      <button>Save</button>
+      <button>Donate</button>
+    </div>
     )
 }
 
