ff71a343293e2b7f97eeb39ffbdf57e7d3bbf0fb
anistone9 <ani.stone.v@gmail.com>
2023-06-05 16:20:42 -0700

    formatted result cards

--- client/src/components/DogSearchList/index.js
+++ client/src/components/DogSearchList/index.js
@@ -7,6 +7,15 @@ import Auth from '../../utils/auth';
 import DogSearchResult from '../DogSearchResult';
 import { QUERY_DOGS } from '../../utils/queries';
 
+import {
+  Container,
+  Col,
+  Form,
+  Button,
+  Card,
+  Row
+} from 'react-bootstrap';
+
 function DogSearchList() {
 
   const { loading, data } = useQuery(QUERY_DOGS);
@@ -22,42 +31,68 @@ function DogSearchList() {
   })
 
   const dogData = data || {};
+  //console.log(dogData.dogs);
 
   return (
-    <div>
-      <select id="dropdown1">
-        <option value="noFilter">Choose Age</option>
-        <option value="option1">Baby</option>
-        <option value="option2">Young</option>
-        <option value="option3">Adult</option>
-      </select>
+    <>
+      <div>
+        <select id="dropdown1">
+          <option value="ageFilter">Choose Age</option>
+          <option value="option1">Baby</option>
+          <option value="option2">Young</option>
+          <option value="option3">Adult</option>
+        </select>
 
-      <select id="dropdown2">
-        <option value="">Choose Gender</option>
-        <option value="option1">Male</option>
-        <option value="option2">Female</option>
-      </select>
+        <select id="dropdown2">
+          <option value="genderFilter">Choose Gender</option>
+          <option value="option1">Male</option>
+          <option value="option2">Female</option>
+        </select>
 
-      <select id="dropdown3">
-        <option value="">Choose Breed</option>
-        <option value="option1">Terrier</option>
-        <option value="option2">Mixed Bred</option>
-        <option value="option3">Golden Retriever</option>
-      </select>
+        <select id="dropdown3">
+          <option value="breedFilter">Choose Breed</option>
+          <option value="option1">Terrier</option>
+          <option value="option2">Mixed Breed</option>
+          <option value="option3">Golden Retriever</option>
+          <option value="option4">German Shepherd</option>
+          <option value="option5">Rottweiler</option>
+          <option value="option6">Scottish Terrier</option>
+          <option value="option7">Labrador Retriever</option>
+          <option value="option8">Westie</option>
+          <option value="option9">Hound</option>
+          <option value="option10">Pit Bull Terrier</option>
+          <option value="option11">Australian Shepherd</option>
+          <option value="option12">Poodle</option>
+          <option value="option13">Pomeranian</option>
+          <option value="option14">Chihuahua</option>
+          <option value="option15">Jack Russell Terrier</option>
+          <option value="option16">Dachshund</option>
+          <option value="option16">Tibetan Spaniel</option>
+          <option value="option16">Pug</option>
+          <option value="option16">Belgian Shepherd</option>
+          <option value="option16">Australian Cattle Dog</option>
+        </select>
+      </div>
 
-     <h2>Bark buddies available to adopt:</h2>
-        {dogData && (
-          <div>
-            {dogData?.dogs?.map(item => (
-              <DogSearchResult key={item.id} dog={item}>
-              </DogSearchResult>
-            ))}
-          </div>
-        )}
-        <DogSearchResult
-            dog={dogData}
-          />
-    </div>
+      <Container>
+        <h3 className='pt-4'>Bark buddies available to adopt:</h3>
+        <Row>
+          {dogData && (
+            <Col>
+              <div className='h-100'>
+                {dogData?.dogs?.map(item => (
+
+                  <DogSearchResult key={item.id} dog={item}>
+                  </DogSearchResult>
+
+                ))}
+              </div>
+            </Col>
+          )}
+        </Row>
+      </Container>
+
+    </>
   )
 };
 
