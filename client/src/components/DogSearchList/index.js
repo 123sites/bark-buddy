77583b774260936c3374622801a794e30341f0b6
anistone9 <ani.stone.v@gmail.com>
2023-06-07 20:07:33 -0700

    working on cleaning up filters

--- client/src/components/DogSearchList/index.js
+++ client/src/components/DogSearchList/index.js
@@ -10,139 +10,177 @@ import { QUERY_DOGS } from '../../utils/queries';
 import {
   Container,
   Col,
-  Form,
   Button,
-  Card,
   Row
 } from 'react-bootstrap';
 
 function DogSearchList({ userData, refetchUser }) {
 
   const { loading, data } = useQuery(QUERY_DOGS);
+  const [dogData, setDogData] = useState(data);
+  const [filteredDogData, setFilteredDogData] = useState(data?.dogs || []);
+  const [selectedBreed, setSelectedBreed] = useState('');
+  const [selectedAge, setSelectedAge] = useState('');
+  const [selectedGender, setSelectedGender] = useState('');
 
-  let [dogData, setDogData] = useState(data);
   useEffect(() => {
     setDogData(data);
+    setFilteredDogData(data?.dogs || []);
   }, [data]);
 
-  //let dogData = data || {};
-
-  //console.log(dogData.dogs);
-  const breeds = [...new Map(dogData?.dogs?.map((dog) => [dog.breed, dog])).values()];
-  //console.log(breeds);
-
-  const ages = [...new Map(dogData?.dogs?.map((dog) => [dog.age, dog])).values()];
-  //console.log(ages);
-
-  const genders = [...new Map(dogData?.dogs?.map((dog) => [dog.gender, dog])).values()];
- //console.log(genders);
+  const handleClearFilters = () => {
+    setSelectedBreed('');
+    setSelectedAge('');
+    setSelectedGender('');
+    setFilteredDogData(dogData?.dogs || []);
+  };
 
   const handleInputBreed = (event) => {
-    console.log(event.target.value);
+    //console.log(event.target.value);
     const targettedAttribute = event.target.value;
-    const newDogArray = dogData?.dogs?.filter((dog) => { return targettedAttribute === dog.breed });
-    setDogData({dogs: newDogArray});
-    console.log(dogData.dogs);
-  }
+    setSelectedBreed(targettedAttribute);
+    const newDogArray = dogData?.dogs?.filter((dog) => {
+      return (
+        (targettedAttribute === '' || targettedAttribute === dog.breed) &&
+        isMatchingAge(dog) &&
+        isMatchingGender(dog)
+      );
+    });
+    setFilteredDogData(newDogArray);
+  };
 
   const handleInputAge = (event) => {
-    console.log(event.target.value);
+    //console.log(event.target.value);
     const targettedAttribute = event.target.value;
-    const newDogArray = dogData?.dogs?.filter((dog) => { return targettedAttribute === dog.age });
-    setDogData({dogs: newDogArray});
-    console.log(dogData);
-  }
+    setSelectedAge(targettedAttribute);
+    const newDogArray = dogData?.dogs?.filter((dog) => {
+      return (
+        (selectedBreed === '' || selectedBreed === dog.breed) &&
+        (targettedAttribute === '' || targettedAttribute === dog.age) &&
+        (selectedGender === '' || selectedGender === dog.gender)
+      );
+    });
+    setFilteredDogData(newDogArray);
+  };
 
   const handleInputGender = (event) => {
-    console.log(event.target.value);
+    //console.log(event.target.value);
     const targettedAttribute = event.target.value;
-    const newDogArray = dogData?.dogs?.filter((dog) => { return targettedAttribute === dog.gender });
-    setDogData({dogs: newDogArray});
-    console.log(dogData);
-  }
+    setSelectedGender(targettedAttribute);
+    const newDogArray = dogData?.dogs?.filter((dog) => {
+      return (
+        (selectedBreed === '' || selectedBreed === dog.breed) &&
+        (selectedAge === '' || selectedAge === dog.age) &&
+        (targettedAttribute === '' || targettedAttribute === dog.gender)
+      );
+    });
+    setFilteredDogData(newDogArray);
+  };
+
+  const isMatchingBreed = (dog) => {
+    return selectedBreed === '' || selectedBreed === dog.breed;
+  };
+
+  const isMatchingAge = (dog) => {
+    return selectedAge === '' || selectedAge === dog.age;
+  };
+
+  const isMatchingGender = (dog) => {
+    return selectedGender === '' || selectedGender === dog.gender;
+  };
+
+  const breeds = [...new Map(dogData?.dogs?.map((dog) => [dog.breed, dog])).values()];
+
+  const ages = [...new Map(dogData?.dogs?.map((dog) => [dog.age, dog])).values()];
+
+  const genders = [...new Map(dogData?.dogs?.map((dog) => [dog.gender, dog])).values()];
 
   return (
     <>
-    <Row>
-      <Col md={3}>
-        <div>
+      <Row>
+        <Col md={2}>
+          <div>
 
-        <div>
-          <Container>
-            <h3 className='pt-4'>Filters</h3>
-          </Container>
-        </div>
+            <div>
+              <Container>
+                <h3 className='pt-4'>Filters</h3>
+              </Container>
+            </div>
+            <br />
 
-          <br></br>
-          <select
-            onChange={handleInputBreed}
-            id="breedFilter">
-            <option>Choose breed</option>
-            {breeds?.map((dog) => {
-              return (
-                <option value={dog.breed}>
-                  {dog.breed}
-                </option>
-              )
-            })}
-          </select>
+            <select
+              onChange={handleInputBreed}
+              id="breedFilter"
+              value={selectedBreed}>
+              <option value="">Choose breed</option>
+              {breeds?.map((dog) => {
+                if (
+                  (selectedAge === '' || selectedAge === dog.age) &&
+                  (selectedGender === '' || selectedGender === dog.gender)
+                ) {
+                  return (
+                    <option key={dog.breed} value={dog.breed}>
+                      {dog.breed}
+                    </option>
+                  );
+                } else {
+                  return null;
+                }
+              })}
+            </select>
+            <br />
 
-          <br></br>
-
-          <select
-            onChange={handleInputAge}
-            id="ageFilter">
-            <option>Choose age</option>
-            {ages?.map((dog) => {
-              return (
-                <option value={dog.age}>
+            <select
+              onChange={handleInputAge}
+              id="ageFilter"
+              value={selectedAge}>
+              <option value="">Choose age</option>
+              {ages?.map((dog) => (
+                <option key={dog.age} value={dog.age}>
                   {dog.age}
                 </option>
-              )
-            })}
-          </select>
+              ))}
+            </select>
+            <br />
 
-          <br></br>
-
-          <select
-            onChange={handleInputGender}
-            id="genderFilter">
-            <option>Choose gender</option>
-            {genders?.map((dog) => {
-              return (
-                <option value={dog.gender}>
+            <select
+              onChange={handleInputGender}
+              id="genderFilter"
+              value={selectedGender}>
+              <option value="">Choose gender</option>
+              {genders?.map((dog) => (
+                <option key={dog.gender} value={dog.gender}>
                   {dog.gender}
                 </option>
-              )
-            })}
-          </select>
+              ))}
+            </select>
+            <br />
 
-        </div>
+            <Button variant="secondary"
+              onClick={handleClearFilters}>Clear all filters</Button>
 
-        <div>
-          <button >Clear all filters</button>
-        </div>
-      </Col>
+          </div>
+        </Col>
 
-      <Col md={9}>
-        <div>
+        <Col md={10}>
+          <div>
+            <Container>
+              <h3 className='pt-4'>Bark buddies available to adopt:</h3>
+              <br />
+            </Container>
+          </div>
+
           <Container>
-            <h3 className='pt-4'>Bark buddies available to adopt:</h3>
-            <br></br>
+            <Row>
+              {filteredDogData?.map((item) => (
+                <Col key={item.id} md="4">
+                  <DogSearchResult dog={item} userData={userData} refetchUser={refetchUser} />
+                </Col>
+              ))}
+            </Row>
           </Container>
-        </div>
-
-        <Container>
-          <Row>
-            {dogData && dogData?.dogs?.map((item) => (
-              <Col key={item.id} md="4">
-                    <DogSearchResult dog={item} userData={userData} refetchUser={refetchUser} />
-                  </Col>
-            ))}
-          </Row>
-        </Container>
-      </Col>
-</Row>
+        </Col>
+      </Row>
     </>
   )
 };
