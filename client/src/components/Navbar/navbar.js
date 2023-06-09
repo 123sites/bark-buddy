a55195081e501e5b845ed6e8ed10b151c8b70ca1
CF <cfreitas12345@gmail.com>
2023-06-08 20:56:19 -0700

    Debugged Heroku error build.

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -1,22 +1,21 @@
 import { React, useContext } from "react"; // willis
 import Logo from "../../images/logo.png";
 import "../../assets/style.css";
-import { ThemeContext } from './themeContext'; // willis
+import { ThemeContext } from "./themeContext"; // willis
 
 export default function Navbar({ handlePageChange, currentPage }) {
   console.log(currentPage);
   const { theme } = useContext(ThemeContext); // willis
   return (
-
     <nav className="flex items-center md:justify-between flex-wrap p-6 sticky">
-      <div className={`Navbar ${theme}`}> {/* willis */}
+      <div className={`Navbar ${theme}`}>
+        {" "}
+        {/* willis */}
         <div className="logo-wrapper">
-          <div className="flex items-center flex-shrink-0 mr-6">
+          <div className="flex items-center flex-shrink-0">
             {/* <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg> */}
             <div>
-              <span className="">
-                Bark Buddy
-              </span>
+              <span className="">Bark Buddy</span>
             </div>
             <div className="block md:hidden">
               <button className="flex items-center px-3 py-2">
@@ -37,7 +36,7 @@ export default function Navbar({ handlePageChange, currentPage }) {
             <a
               href="#home"
               onClick={() => handlePageChange("Home")}
-              className="block mt-4 md:inline-block md:mt-0 text-blueGray nav-link mr-5"
+              className="block mt-4 md:inline-block md:mt-0 text-blueGray nav-link"
             >
               Trying to find the right dog?
             </a>
@@ -45,7 +44,7 @@ export default function Navbar({ handlePageChange, currentPage }) {
             <a
               href="#dog"
               onClick={() => handlePageChange("Dog")}
-              className="block mt-4 md:inline-block md:mt-0 text-blueGray nav-link mr-5"
+              className="block mt-4 md:inline-block md:mt-0 text-blueGray nav-link"
             >
               Pick A Pooch
             </a>
@@ -53,7 +52,7 @@ export default function Navbar({ handlePageChange, currentPage }) {
             <a
               href="#login"
               onClick={() => handlePageChange("Login")}
-              className="block mt-4 md:inline-block md:mt-0 text-blueGray nav-link mr-5"
+              className="block mt-4 md:inline-block md:mt-0 text-blueGray nav-link"
             >
               Login
             </a>
@@ -61,7 +60,7 @@ export default function Navbar({ handlePageChange, currentPage }) {
             <a
               href="#logout"
               onClick={() => handlePageChange("Logout")}
-              className="block mt-4 md:inline-block md:mt-0 text-blueGray nav-link mr-5"
+              className="block mt-4 md:inline-block md:mt-0 text-blueGray nav-link"
             >
               Logout
             </a>
@@ -69,17 +68,14 @@ export default function Navbar({ handlePageChange, currentPage }) {
             <a
               href="#videos"
               onClick={() => handlePageChange("Videos")}
-              className="block mt-4 md:inline-block md:mt-0 text-blueGray nav-link mr-5"
+              className="block mt-4 md:inline-block md:mt-0 text-blueGray nav-link"
             >
               Videos
             </a>
-
           </div>
         </div>
-      </div> {/* willis */}
+      </div>{" "}
+      {/* willis */}
     </nav>
   );
 }
-
-
-
