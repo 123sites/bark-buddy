ed4794fa94d0497def58cade98ba8ccf76fcc3e9
[Grant Willis] <[thegwillis.gw@gmail.com]>
2023-06-10 12:10:48 -0700

    Imported Dobby.png, saving work on donations page

--- client/src/components/Payments/Stripe.js
+++ client/src/components/Payments/Stripe.js
@@ -1,14 +1,27 @@
 import React from "react";
 import "../../assets/style.css";
+import Dobby from "../../images/Dobby.png"; 
 
 const stripe = () => {
   return (
     <div className="container-fluid">
       <div className="row m-5">
+      
         <h2 className="donate-title mt-5 mb-3 fw-bolder fs-2">
           <i class="fa-solid fa-circle-dollar-to-slot mr-5"></i>
-          Would you like to Donate $1?
+          Please Consider Donating:
         </h2>
+        <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center,"
+        }}>
+          <img src={Dobby} 
+          alt="Beautiful Golden Retriever" 
+          style={{width: "200px", height: "auto"}}
+          /> 
+        </div>
         <div>
           <a
             href="https://buy.stripe.com/test_6oEg1m8Ly2ES81y288"
@@ -27,6 +40,7 @@ const stripe = () => {
         </div>
       </div>
     </div>
+     
   );
 };
 
